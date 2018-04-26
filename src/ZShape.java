49c7739f51d4606c39342ee73715e28447d78f6f
Toddy <newdrop@live.com>
2018-04-26 15:05:30 -0500

    Changed Zshape

--- src/ZShape.java
+++ src/ZShape.java
@@ -3,6 +3,9 @@ import java.awt.Color;
 public class ZShape extends AbstractPiece {
 
 
+	public void worthlessMethod() {
+		
+	}
   public ZShape(int row, int col, Grid grid) {
     super(row, col, grid);
     this.square[0] = new Square(grid, row, col -1, Color.red, true);
